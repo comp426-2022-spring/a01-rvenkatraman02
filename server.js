d2e45062d737d37b0762fac65f9a20f02b925887
rvenkatraman02 <rvenka@ad.unc.edu>
2022-01-27 15:44:11 -0500

    moving server block inside readfile

--- server.js
+++ server.js
@@ -20,23 +20,23 @@ fs.readFile('./www/index.html', 'utf8' , (err, data) => {
     if (err) {
         console.error(err)
         return
+        process.exit(1)
     }
-    console.log(data)
-})
 // Define a const `server` as an arrow function using http.createServer. 
 // Use the documentation for the node.js http module. 
 // The function should have three responses: 
 // 1. status code 200, 
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./www/index.html.
-const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html')
-    res.end('./www/index.html')
-  })
+    const server = http.createServer((req, res) => {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/html')
+        res.end(data)
+    })
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
-server.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
-  })
+    server.listen(port, () => {
+        console.log(`Server listening on port ${port}`)
+    })
+})
 // That's it! You're all done!
